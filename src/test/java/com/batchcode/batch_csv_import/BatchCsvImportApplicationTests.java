cd83b6ee24a731fe2a3d1850a668b14956f7fd69
Vijender-Kumar <vijbirhman2017@gmail.com>
2025-04-07 15:14:01 +0530

    Base Setup Done

---
+++ src/test/java/com/batchcode/batch_csv_import/BatchCsvImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batchcode.batch_csv_import;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchCsvImportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
