cf6f7442aa1a8f83e8684cac46f787e3f3db8fb7
Vijender-Kumar <vijbirhman2017@gmail.com>
2025-04-11 12:08:31 +0530

    User Model Udpated

--- src/main/java/com/batchcode/batch_csv_import/model/User.java
+++ src/main/java/com/batchcode/batch_csv_import/model/User.java
@@ -1,22 +1,30 @@
 package com.batchcode.batch_csv_import.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "USER_DATA")
+@Table(name = "user_data")
 @Data
 public class User {
 
     @Id
     private int id;
+    @Column(name = "firstName")
     private String firstName;
+    @Column(name = "lastName")
     private String lastName;
+    @Column(name = "email")
     private String email;
+    @Column(name = "gender")
     private String gender;
+    @Column(name = "contactNo")
     private String contactNo;
+    @Column(name = "country")
     private String country;
+    @Column(name = "dob")
     private String dob;
 }
